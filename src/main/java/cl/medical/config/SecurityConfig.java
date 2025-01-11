3239933b0b64724f904da546c67089d702ae193c
O009920 <mfuentes@forum.cl>
2025-01-11 20:19:30 -0300

    fix cors

--- src/main/java/cl/medical/config/SecurityConfig.java
+++ src/main/java/cl/medical/config/SecurityConfig.java
@@ -11,16 +11,16 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.Arrays;
 
-@Configuration
-@EnableWebSecurity
+//@Configuration
+//@EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        http.cors(Customizer.withDefaults()).authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
-                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
-        return http.build();
-    }
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//
+//        http.cors(Customizer.withDefaults()).authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+//                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+//        return http.build();
+//    }
 
 }
