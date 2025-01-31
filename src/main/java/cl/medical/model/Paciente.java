ad4c22bdb48ee0a463d56ffeb880b0d61facb243
matfuentesv <mat.fuentes@duocuc.cl>
2025-01-31 12:00:28 -0300

    fix campos tablas

--- src/main/java/cl/medical/model/Paciente.java
+++ src/main/java/cl/medical/model/Paciente.java
@@ -22,8 +22,14 @@ public class Paciente {
     @Column(name = "apellido")
     private String apellido;
 
+    @Column(name = "rut")
+    private String rut;
+
+    @Column(name = "fechaNacimiento")
+    private String fechaNacimiento;
+
     @Column(name = "edad")
-    private String edad;
+    private int edad;
 
     @Column(name = "direccion")
     private String direccion;
@@ -35,59 +41,5 @@ public class Paciente {
     private String alertaNivel;
 
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getEdad() {
-        return edad;
-    }
-
-    public void setEdad(String edad) {
-        this.edad = edad;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
-    public String getAlertaNivel() {
-        return alertaNivel;
-    }
-
-    public void setAlertaNivel(String alertaNivel) {
-        this.alertaNivel = alertaNivel;
-    }
 }
